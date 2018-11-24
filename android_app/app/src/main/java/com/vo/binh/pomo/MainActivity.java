18a71f914f22da91e4c7debdf6ff3b59b8ebab0e
iismail1997 <2015.ismail@gmail.com>
2018-11-24 16:02:31 -0600

    Removed Action Bar, added member vars, and fixed button sizes

--- android_app/app/src/main/java/com/vo/binh/pomo/MainActivity.java
+++ android_app/app/src/main/java/com/vo/binh/pomo/MainActivity.java
@@ -2,12 +2,22 @@ package com.vo.binh.pomo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    // Member Variables (Global)
+    Button mTimerButton;
+    Button mPlannerButton;
+    Button mStatisticsButoon;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Views
+        mTimerButton = findViewById(R.id.btn_timer);
+        mPlannerButton = findViewById(R.id.btn_planner);
+        mStatisticsButoon = findViewById(R.id.btn_statistics);
     }
 }
