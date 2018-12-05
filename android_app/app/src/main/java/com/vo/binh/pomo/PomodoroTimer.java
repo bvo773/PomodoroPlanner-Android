8e018047fdba97ffb98baf9067de4de10f85f33a
Binh Vo <binhvo07@gmail.com>
2018-12-04 20:32:36 -0500

    Sketched out designs for layouts using sketch and updated Pomodoro Timer to display in mins/secs

--- android_app/app/src/main/java/com/vo/binh/pomo/PomodoroTimer.java
+++ android_app/app/src/main/java/com/vo/binh/pomo/PomodoroTimer.java
@@ -8,11 +8,15 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
 public class PomodoroTimer extends AppCompatActivity {
     TextView mTimerView;
     Button mStartPauseButton;
     Button mStopButton;
     CountDownTimer timer;
+    final int INTERVAL25MINS = 1501000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,11 +31,14 @@ public class PomodoroTimer extends AppCompatActivity {
         mStartPauseButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                timer = new CountDownTimer(30000, 1000) {
+                timer = new CountDownTimer(INTERVAL25MINS, 1000) {
                     @Override
                     public void onTick(long millisUntilFinished) {
-                        Log.d("Timer", "seconds remaining: " + millisUntilFinished/1000);
-                        mTimerView.setText(Long.toString(millisUntilFinished/1000));
+                        String text = String.format(Locale.getDefault(), "Time Remaining %02d min: %02d sec",
+                                TimeUnit.MILLISECONDS.toMinutes(millisUntilFinished) % 60,
+                                TimeUnit.MILLISECONDS.toSeconds(millisUntilFinished) % 60);
+                        mTimerView.setText(text);
+                        Log.d("timer", text);
                     }
 
                     @Override
