f9b4fc169512ff16230c7572a2f3a02f23667ebc
Binh Vo <binhvo@Binhs-MacBook-Pro.local>
2019-04-15 13:39:56 -0400

    Working on Statistics

--- android_app/app/src/main/java/com/vo/binh/pomo/BarGraphActivity.java
+++ android_app/app/src/main/java/com/vo/binh/pomo/BarGraphActivity.java
@@ -1,12 +1,15 @@
 package com.vo.binh.pomo;
 
+import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.github.mikephil.charting.charts.BarChart;
 
-public class BarGraphActivity {
+public class BarGraphActivity extends AppCompatActivity {
 
     private BarChart mBarChart;
     private TextView tvX, tvY;
+
+
 
 }
